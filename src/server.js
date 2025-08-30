239a906e8ac1a56710c0839baff097030a72c636
Mudit Rajput <muditrajputpersonal@gmail.com>
2025-08-30 23:39:40 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -13,7 +13,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
-app.use(cors());
+app.use(cors({
+  origin: "https://app-frontend-livid.vercel.app",  // exact frontend origin
+  credentials: true
+}));
+
    
 
 app.use(express.json());
